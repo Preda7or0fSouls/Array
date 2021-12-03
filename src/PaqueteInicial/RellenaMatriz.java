0f7cbfcb5b6477010e9a2f0d73b72cdca543cc9d
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2021-12-03 08:45:28 +0100

    version 1.2

---
+++ src/PaqueteInicial/RellenaMatriz.java
@@ -0,0 +1,7 @@
+package PaqueteInicial;
+public class RellenaMatriz {
+    /*Rellena una matriz de 10 x 10 con valores aleatorios (0 o 1) e imprímela de la siguiente forma:*/
+    public static void main(String[] args) {
+        
+    }
+}
