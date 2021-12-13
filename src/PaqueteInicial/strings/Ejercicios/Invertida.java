9859eec031a81e834b030311b4017864f24546d8
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2021-12-13 09:26:18 +0100

    version 1.0 invertida

--- src/PaqueteInicial/strings/Ejercicios/Invertida.java
+++ src/PaqueteInicial/strings/Ejercicios/Invertida.java
@@ -1,5 +1,20 @@
 package Ejercicios;
 
-public class Invertida {
+import Utiles.Utilidades;
 
+public class Invertida {
+    public static String inversion(String[] palabras) {
+        String invertida = "";
+        int i;
+        for (i = palabras.length - 1; i >= 0; i--) {
+            invertida = invertida + palabras[i] + " ";
+        }
+        return invertida;
+    }
+
+    public static void main(String[] args) {
+        String invertida = inversion(
+                Utiles.Utilidades.dividirEnPalabras(Utiles.Utilidades.leerCadena("Introduce texto: ")));
+        System.out.println(invertida);
+    }
 }
