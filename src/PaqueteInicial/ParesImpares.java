724a55f040ccd83845b1d503c84df5d6949af50c
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2021-12-15 08:32:25 +0100

    paresimpares terminado

--- src/PaqueteInicial/ParesImpares.java
+++ src/PaqueteInicial/ParesImpares.java
@@ -1,4 +1,16 @@
 package PaqueteInicial;
+
 public class ParesImpares {
-    
-}
+    public static void main(String[] args) {
+        int [] numeros = {2,4,6,7};
+        int contadorPares = 0;
+        int contadorImpares = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros [i] !=2) {
+                contadorImpares++;
+            }else{
+                contadorPares++;
+            }
+        }System.out.println( "Hay" + contadorImpares + "impares" + "y" + contadorPares + "pares");
+    }
+}}}
