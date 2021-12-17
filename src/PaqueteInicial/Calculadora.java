c2bac0ccf921f8ddd4c8d1b30dc75ee2d8f06299
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2021-12-17 10:36:21 +0100

    version caculadora 1.0.1

--- src/PaqueteInicial/Calculadora.java
+++ src/PaqueteInicial/Calculadora.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 
 public class Calculadora {
 
+    String result;
+
+    public static double Suma() {
+        return result;
+    }
+
 }
 
 public static void main(String[] args) {
-    
+    Scanner entrada=new Scanner(System.in);
+    String op;
+    System.out.print("Introduce la operacion a realizar");
+    op = entrada.next();
+
 }
