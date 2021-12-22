f11ddd6dd9d721a5f8b22d4fc4f06b28ce7d1ca5
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2021-12-22 09:04:38 +0100

    chorichat version inicial

--- src/PaqueteInicial/Calculadora.java
+++ src/PaqueteInicial/Calculadora.java
@@ -1,21 +1,95 @@
 package PaqueteInicial;
 
-import java.util.Scanner;
-
 public class Calculadora {
 
-    String result;
+    public static void main(String[] args) {
+        String linea;
 
-    public static double Suma() {
-        return result;
+        linea = Utilidades.leerCadena("-->");
+
+        while (!esFin(linea)) {
+            if (esOperacionValida(linea)) {
+                System.out.println(calcularResultado(linea));
+            } else {
+                System.out.println("Operación inválida");
+            }
+            linea = Utiles.Utilidades.leerCadena("-->");
+        }
     }
 
-}
+    private static double calcularResultado(String linea) {
+        String[] elementos = Utilidades.dividirEnPalabras(linea);
+        double resultado = 0.0;
 
-public static void main(String[] args) {
-    Scanner entrada=new Scanner(System.in);
-    String op;
-    System.out.print("Introduce la operacion a realizar");
-    op = entrada.next();
+        double op1 = Double.parseDouble(elementos[0]);
+        double op2 = Double.parseDouble(elementos[2]);
 
+        switch (elementos[1]) {
+            case "+":
+                resultado = op1 + op2;
+                break;
+            case "-":
+                resultado = op1 - op2;
+                break;
+            case "x":
+            case "X":
+                resultado = op1 * op2;
+                break;
+            case "/":
+                resultado = op1 / op2;
+                break;
+            default:
+        }
+
+        return resultado;
+    }
+
+    public static boolean esOperacionValida(String linea) {
+        String[] elementos = Utilidades.dividirEnPalabras(linea);
+
+        if (elementos.length == 3) {
+            if (isDouble(elementos[0]) &&
+                    isOperator(elementos[1]) &&
+                    isDouble(elementos[2])) {
+                return true;
+
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isOperator(String operator) {
+        switch (operator) {
+            case "+":
+            case "-":
+            case "x":
+            case "X":
+            case "/":
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public static boolean esFin(String linea) {
+        // Fin, fin, FiN, FIN...
+        String minusculas = linea.toLowerCase();
+        if (minusculas.equals("fin")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isDouble(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException error) {
+            return false;
+        }
+    }
 }
---
+++ src/PaqueteInicial/ChoriChatBot.java
@@ -0,0 +1,11 @@
+package PaqueteInicial;
+
+public class ChoriChatBot {
+
+    public static void main(String[] args) {
+        String charla; // interacciones del usuario
+        String str = "ChoriChat>\n ";
+        System.out.println(str.repeat(10));
+
+    }
+}
