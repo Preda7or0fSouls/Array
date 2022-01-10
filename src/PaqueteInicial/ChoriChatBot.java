a825899987404eb97a1a48955a3d48cc010bfc25
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2022-01-10 07:59:41 +0100

    v: 1.1

--- src/PaqueteInicial/ChoriChatBot.java
+++ src/PaqueteInicial/ChoriChatBot.java
@@ -1,11 +1,30 @@
 package PaqueteInicial;
 
+import java.util.Scanner;
+
 public class ChoriChatBot {
+    final static int CALLADO = 0;
+    final static int CHARLA = 1;
+    final static int REPETICION = 2;
+    final static int FIN = 3;
 
     public static void main(String[] args) {
-        String charla; // interacciones del usuario
-        String str = "ChoriChat>\n ";
-        System.out.println(str.repeat(10));
+        Scanner entrada = new Scanner(System.in);
 
+        int modo = CALLADO;
+
+        while (modo != FIN) {
+            System.out.print("ChoriChat> ");
+            String linea = entrada.nextLine().toLowerCase();
+            modo = updateModo(modo, linea);
+        }
+        entrada.close();
+
+    }
+
+    private static void Impresion(int modo) {
+        switch (modo){
+            case = "CALLADO"
+       }
     }
 }
