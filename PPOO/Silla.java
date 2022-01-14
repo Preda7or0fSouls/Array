becfccafa1795f649b542ffd392ceabe89bf87d7
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2022-01-14 10:41:57 +0100

    12-1-22 11

---
+++ PPOO/Silla.java
@@ -0,0 +1,9 @@
+package PPOO;
+
+public class Silla {
+    privatw String color;
+    private int numPatas;
+    private String material;
+    private boolean respaldo;
+    private byte Nivelcomodidad;
+}
