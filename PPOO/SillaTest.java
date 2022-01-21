6f321c1f944bf1dd890a15196aa7c8f1187ebd12
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2022-01-21 09:42:16 +0100

    21-01-21 silla completa

---
+++ PPOO/SillaTest.java
@@ -0,0 +1,17 @@
+package PPOO;
+import PPOO.Silla;
+import  java.util.Random;
+public class Test2 {
+    public static void main(String[] args) {
+        int Silla = 0;
+        for (int  Silla = 0; i < 100; i++) {
+            if( Silla=<100){
+                Random Silla = new Random();
+                System.out.println(Silla);
+            } else{
+                System.out.print("Hay demasiadas sillas");
+            }
+        }
+
+    }
+}
--- PPOO/Test2.java
+++
@@ -1,7 +0,0 @@
-package PPOO;
-
-public class Test2 {
-    public static void main(String[] args) {
-
-    }
-}
