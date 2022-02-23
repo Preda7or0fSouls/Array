ccd29fbf63e45e14fe7a50cf5c6436f5c5bad3d2
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2022-02-23 10:36:17 +0100

    23 de febrero

---
+++ src/PPOO/Ejercicios/MusicOrganizer.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+
+import javax.sound.midi.Track;
+
+/**
+ * A class to hold details of audio files.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29
+ */
+public class MusicOrganizer
+{
+    // An ArrayList for storing the file names of music files.
+    private ArrayList<String> files;
+      
+    /**
+     * Create a MusicOrganizer
+     */
+    public MusicOrganizer()
+    {
+        files = new ArrayList<String>();
+    }
+    
+    /**
+     * Add a file to the collection.
+     * @param filename The file to be added.
+     */
+    public void addFile(String filename)
+    {
+        files.add(filename);
+    }
+    
+    /**
+     * Return the number of files in the collection.
+     * @return The number of files in the collection.
+     */
+    public int getNumberOfFiles()
+    {
+        return files.size();
+    }
+    
+    /**
+     * List a file from the collection.
+     * @param index The index of the file to be listed.
+     */
+    public void listFile(int index)
+    {
+        if(isValidIndex(index)) {
+            String filename = files.get(index);
+            System.out.println(filename);
+        }
+    }
+    
+    /**
+     * Remove a file from the collection.
+     * @param index The index of the file to be removed.
+     */
+    public void removeFile(int index)
+    {
+        if(isValidIndex(index)) {
+            files.remove(index);
+        }
+    }
+
+    public void checkIndex(int num){
+        if((num < 0) || (num >= files.size())){
+            System.out.println("El rango válido de valores está entre 0 y " + (files.size()-1));
+        }
+    }
+
+    public boolean isValidIndex(int num){
+
+        boolean resultado;
+
+        if((num < 0) || (num >= files.size())){
+            resultado = false;
+        }else{
+            resultado = true;
+        }
+
+        return resultado;
+    }
+}
---
+++ src/PPOO/Ejercicios/music-organizer-v2/MusicOrganizer.java
@@ -0,0 +1,147 @@
+import java.util.ArrayList;
+
+/**
+ * A class to hold details of audio files.
+ * This version can play the files.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29
+ */
+public class MusicOrganizer
+{
+    // An ArrayList for storing the file names of music files.
+    private ArrayList<String> files;
+    // A player for the music files.
+    private MusicPlayer player;
+        
+    /**
+     * Create a MusicOrganizer
+     */
+    public MusicOrganizer()
+    {
+        files = new ArrayList<>();
+        player = new MusicPlayer();
+    }
+    
+    /**
+     * Add a file to the collection.
+     * @param filename The file to be added.
+     */
+    public void addFile(String filename)
+    {
+        files.add(filename);
+    }
+    
+    /**
+     * Return the number of files in the collection.
+     * @return The number of files in the collection.
+     */
+    public int getNumberOfFiles()
+    {
+        return files.size();
+    }
+    
+    /**
+     * List a file from the collection.
+     * @param index The index of the file to be listed.
+     */
+    public void listFile(int index)
+    {
+        if(index >= 0 && index < files.size()) {
+            String filename = files.get(index);
+            System.out.println(filename);
+        }
+    }
+    
+    /**
+     * Remove a file from the collection.
+     * @param index The index of the file to be removed.
+     */
+    public void removeFile(int index)
+    {
+        if(index >= 0 && index < files.size()) {
+            files.remove(index);
+        }
+    }
+
+    /**
+     * Start playing a file in the collection.
+     * Use stopPlaying() to stop it playing.
+     * @param index The index of the file to be played.
+     */
+    public void startPlaying(int index)
+    {
+        String filename = files.get(index);
+        player.startPlaying(filename);
+    }
+
+    /**
+     * Stop the player.
+     */
+    public void stopPlaying()
+    {
+        player.stop();
+    }
+
+    public void listAllFiles(){
+        for (String cancion : files) {
+            System.out.println(cancion);
+        }
+    }
+
+    public void listMatching(String match){
+
+        for (String cancion : getMatching(match)) {    
+            System.out.println(cancion);
+        }
+
+        if (getMatching(match).size() == 0){
+            System.out.println("No se ha encontrado ninguna canción coincidente");
+        }
+    }
+
+    public void playMatching(String match){
+
+        ArrayList<String> canciones = getMatching(match);
+
+        for (String cancion : canciones) {
+            System.out.println(cancion);
+            player.playSample(cancion);
+        }
+    }
+
+    public ArrayList<String> getMatching(String match){
+        
+        ArrayList<String> resultado = new ArrayList<>();
+
+        for (String cancion : files) {
+            if(cancion.contains(match)){
+                resultado.add(cancion);
+            }
+        }
+
+        return resultado;
+    }
+
+    public int findFirst(String match){
+
+        int indice = 0;
+        int resultado = -1;
+
+        while (indice < files.size()) {
+            
+            if(files.get(indice).contains(match)){
+                
+                
+                break;
+            }else{
+                indice++;
+            }
+        }
+
+        return resultado;
+
+    }
+
+
+}
