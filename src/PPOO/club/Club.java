d4c5a3f6ffde0ed4fb86a54058658c2a78643a3a
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2022-03-09 10:12:26 +0100

    9 de marzo

---
+++ src/PPOO/club/Club.java
@@ -0,0 +1,49 @@
+/**
+ * Store details of club memberships.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+ import java.util.ArrayList;
+ import java.util.List;
+public class Club
+{
+    // Define any necessary fields here ...
+
+    /**
+     * Constructor for objects of class Club
+     */
+    public Club()
+    {
+        // Initialise any fields here ...
+         List<String> members = new ArrayList<>();
+    }
+
+    /**
+     * Add a new member to the club's list of members.
+     * @param member The member object to be added.
+     */
+    public void join(Membership member)
+    {
+    }
+
+    /**
+     * @return The number of members (Membership objects) in
+     *         the club.
+     */
+    public int numberOfMembers()
+    {
+        return members;
+    }
+    /**
+    * Determinar el numero de miembros que se han unido en
+    * el mes indicado
+    * @param month El mes que nos interesa
+    * @return El n mero de miembros que se han unido
+    * ese mes
+    */
+
+    public int joinedInMonth(int month){
+    return 
+    }
+}
