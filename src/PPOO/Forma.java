f17b9a883b26f904b0dd73cc56b6dbc971170d20
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2022-03-11 10:16:56 +0100

    11-03-22

--- src/PPOO/Forma.java
+++ src/PPOO/Forma.java
@@ -1,12 +1,18 @@
 import java.awt.Point;
 
 public class Forma {
-private String color;
-private Point cordenada;
-private String NombreForma ;
+    private String color;
+    private Point cordenada;
+    private String NombreForma;
 
+    public void Imprimir() {
+    }
 
-public void Imprimir(){
-}
+    public String getColor() {
+        return color;
+    }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
