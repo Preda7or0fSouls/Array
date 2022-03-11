35d4f46001c5dfd8fa35eaa33ae35ba3aeb5c616
charly <49226878+Preda7or0fSouls@users.noreply.github.com>
2022-03-11 10:12:54 +0100

    22.03.22

---
+++ src/PPOO/Forma.java
@@ -0,0 +1,12 @@
+import java.awt.Point;
+
+public class Forma {
+private String color;
+private Point cordenada;
+private String NombreForma ;
+
+
+public void Imprimir(){
+}
+
+}
--- src/PPOO/app/Usuario.java
+++ src/PPOO/app/Usuario.java
@@ -1,11 +1,17 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputFilter.Config;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Usuario implements ParserXML {
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class Usuario implements ParserXML{
     private int id;
     private String email;
     private String password;
@@ -13,25 +19,26 @@ public class Usuario implements ParserXML {
     public static ArrayList<Usuario> list = new ArrayList<>();
     private static int idGenerator = 0;
 
+
     public Usuario(String email, String password) {
 
         this.password = password;
 
-        if (this.isEmailAvailable(email)) {
+        if (this.isEmailAvailable(email)){
             this.id = idGenerator++;
             this.email = email;
             list.add(this);
-        } else {
+        }else{
             this.id = -1;
             this.email = "Correo duplicado: " + email;
         }
     }
 
-    private boolean isEmailAvailable(String email) {
+    private boolean isEmailAvailable(String email){
         boolean resultado = true;
 
         for (Usuario usuario : list) {
-            if (email.equals(usuario.getEmail())) {
+            if(email.equals(usuario.getEmail())){
                 resultado = false;
                 break;
             }
@@ -40,32 +47,29 @@ public class Usuario implements ParserXML {
         return resultado;
     }
 
-    public static int getId() {
+    public int getId() {
         return id;
     }
 
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public static boolean removeOneByName(String email) {
+    public static boolean removeOneByName(String email){
 
         boolean resultado = false;
 
         for (Usuario person : list) {
-            if (person.email.equals(email)) {
+            if (person.email.equals(email)){
                 list.remove(person);
                 resultado = true;
                 break;
@@ -75,21 +79,22 @@ public class Usuario implements ParserXML {
         return resultado;
     }
 
-    public static void removeAllByDomain(String domain) {
+    public static void removeAllByDomain(String domain){
 
         Iterator<Usuario> it = list.iterator();
 
-        while (it.hasNext()) {
+        while(it.hasNext()){
             Usuario user = it.next();
 
-            if (user.email.endsWith(domain)) {
+            if (user.email.endsWith(domain)){
                 list.remove(user);
             }
         }
 
     }
 
-    public static String generateXML() {
+
+    public String generateXML() {
         String xml = "<usuario>\n";
         xml += "<id>" + id + "</id>\n";
         xml += "<password>" + password + "</password>\n";
@@ -98,19 +103,56 @@ public class Usuario implements ParserXML {
         return xml;
     }
 
-    public static String writexml() {
-        String file = Config.userFolder + getId() + ".xml";
-        File ficher = new File(file);
+    public void writeXML(){
+        String filename = Config.userFolder + getId() + ".xml";
+        File fichero = new File(filename);
 
         try {
-            ficher.createNewFile();
-            FileWriter fw = new FileWriter(ficher);
+            fichero.createNewFile();
+            FileWriter fw = new FileWriter(fichero);
             fw.write(generateXML());
             fw.close();
-
         } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
+    }
+
+    public static void loadXML(){
+         File folder = new File("./usuarios");
+
+         list.clear();
+
+         for (File xmlFile : folder.listFiles()) {
+             list.add(getLoadSingleXML(xmlFile));
+         }
+    }
+
+    private static Usuario getLoadSingleXML(File xmlFile) {
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder;
+        Document doc;
+        Usuario newUser = null;
+
+        try {
+            dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(xmlFile);
+            String email = doc.getElementsByTagName("email").item(0).getTextContent();
+            String password = doc.getElementsByTagName("password").item(0).getTextContent();
+            newUser = new Usuario(email, password);
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return newUser;
     }
 }
